3577de3fea3022c867052b3db2476f54e7eeb272
Chris Wren <dazedpandas@gmail.com>
2022-03-11 22:01:49 +0000

    Setup Storybook and Sass

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,39 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/preset-scss",
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "webpack5"
-  }
-}
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+  webpackFinal: async (config, { configType }) => {
+    // get index of css rule
+    const ruleCssIndex = config.module.rules.findIndex(
+      (rule) => rule.test.toString() === "/\\.css$/"
+    );
+
+    // map over the 'use' array of the css rule and set the 'module' option to true
+    config.module.rules[ruleCssIndex].use.map((item) => {
+      if (item.loader && item.loader.includes("/css-loader/")) {
+        item.options.modules = {
+          mode: "local",
+          localIdentName:
+            configType === "PRODUCTION"
+              ? "[local]__[hash:base64:5]"
+              : "[name]__[local]__[hash:base64:5]",
+        };
+      }
+
+      return item;
+    });
+
+    return config;
+  },
+};
